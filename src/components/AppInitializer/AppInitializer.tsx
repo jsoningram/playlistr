3ade3c59d1e5aff0f59759ccae5629400c98e257
Jason Ingram <jason_ingram@motortrend.com>
2025-02-11 16:21:01 -0800

    Layout adjustments

---
+++ src/components/AppInitializer/AppInitializer.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { type FunctionComponent } from 'react';
+
+const AppInitializer: FunctionComponent = () => {
+  // @todo determine how this could be leveraged
+
+  return null;
+};
+
+export default AppInitializer;
